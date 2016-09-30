aeb39d8614f192a105ebc251f8aa8580088ff3a0
weihaodong <macbook@weihaodongdeMacBook-Pro.local>
2016-09-30 15:12:24 +0800

    初始化

---
+++ pages/index/index.js
@@ -0,0 +1,40 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.redirectTo({
+      url: '../order/order'
+    })
+  },
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  onLogin: function () {
+    // wx.request({
+    //   url: 'test.php',
+    //   data: {
+    //     x: '' ,
+    //     y: ''
+    //   },
+    //   header:{
+    //       "Content-Type":"application/json"
+    //   },
+    //   success: function(res) {
+    //     var data = res.data;
+    //   }
+    // });
+  }
+})
